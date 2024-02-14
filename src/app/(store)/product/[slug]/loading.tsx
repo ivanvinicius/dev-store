b444b02ab0c52373f1bb664c0b2e49d9c667c871
ivanvinicius <ivanvinicius@github.com>
2024-02-14 17:16:40 -0300

    :sparkles: fetch de dados no produto

---
+++ src/app/(store)/product/[slug]/loading.tsx
@@ -0,0 +1,5 @@
+export default function ProductLoading() {
+  return (
+    <span className="font-bold text-emerald-500">Carregando produto...</span>
+  )
+}
--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,11 +1,32 @@
 import Image from 'next/image'
 
-export default function Product() {
+import { api } from '~/data/api'
+import { Product } from '~/data/types/product'
+
+interface Props {
+  params: { slug: string }
+}
+
+async function getProduct(slug: string): Promise<Product> {
+  const response = await api(`/products/${slug}`, {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  })
+
+  const parsedProduct = await response.json()
+
+  return parsedProduct
+}
+
+export default async function Product({ params }: Props) {
+  const product = await getProduct(params.slug)
+
   return (
     <div className="relative grid max-h-[860px] grid-cols-3">
       <div className="col-span-2 overflow-hidden">
         <Image
-          src="/moletom-never.png"
+          src={product.image}
           alt=""
           width={1000}
           height={1000}
@@ -14,17 +35,15 @@ export default function Product() {
       </div>
 
       <div className="flex flex-col justify-center px-12">
-        <h1 className="text-3xl font-bold leading-tight">
-          Moletom Never Stop Learning
-        </h1>
+        <h1 className="text-3xl font-bold leading-tight">{product.title}</h1>
 
         <p className="mt-2 leading-relaxed text-zinc-400">
-          Moletom fabricado com 88% de algodão e 12% de poliéster
+          {product.description}
         </p>
 
         <div className="mt-8 flex items-center gap-3">
           <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
-            {Number(129).toLocaleString('pt-BR', {
+            {product.price.toLocaleString('pt-BR', {
               style: 'currency',
               currency: 'BRL',
               maximumFractionDigits: 0,
@@ -32,7 +51,7 @@ export default function Product() {
           </span>
           <span className="text-sm text-zinc-400">
             Em 12x s/ juros de{' '}
-            {Number(129 / 12).toLocaleString('pt-BR', {
+            {Number(product.price / 12).toLocaleString('pt-BR', {
               style: 'currency',
               currency: 'BRL',
               maximumFractionDigits: 2,
